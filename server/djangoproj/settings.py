c0300f8fbe736a7def59e238d5d3fa5692d75229
johnartmx <johnartmx@gmail.com>
2024-06-08 13:03:56 -0600

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://johnartmx-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01. \
+                 'https://johnartmx-8000. \
+                 theiadockernext-0-labs-prod-theiak8s-4-tor01. \
                  proxy.cognitiveclass.ai/']
 CSRF_TRUSTED_ORIGINS = ['https://johnartmx-8000. \
 theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
@@ -97,7 +98,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation. \
+        UserAttributeSimilarityValidator',
     },
     {
         'NAME':
